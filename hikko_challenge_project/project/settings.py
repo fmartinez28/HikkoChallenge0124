79fd97766a971c3e9275fe897cb7fbb30dc0e63c
Facundo Martínez <facusml16@gmail.com>
2024-01-12 17:31:36 -0300

    Eliminado archivo sqlite, modificado settings.py para utilizar postgresql e importar variables de entorno con dotenv

--- hikko_challenge_project/project/settings.py
+++ hikko_challenge_project/project/settings.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+from dotenv import load_dotenv
 from pathlib import Path
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,9 +79,13 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": f"{DB_NAME}",
+        "USER": f"{DB_USER}",
+        "PASSWORD": f"{DB_PASSWORD}",
+        "HOST": f"{DB_HOST}",
+        "PORT": f"{DB_PORT}",
     }
 }
 
